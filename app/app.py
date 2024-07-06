0c6d179982d131df15664362dd06a09f37aa300e
Matthias Pinnewaert <pinnewaert.matthias@gmail.com>
2024-07-06 09:11:32 +0200

    added location and email service

--- app/app.py
+++ app/app.py
@@ -1,8 +1,19 @@
-from flask import Flask, render_template
-
-app = Flask(__name__, static_folder="assets")
+from flask import render_template
+from config import app, mail
+from flask_mail import Mail, Message
 
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+
+@app.route("/send-rsvp")
+def send_rsvp():
+    mail_message = Message(
+        "Hello!",
+        recipients=["pinnewaert@gmail.com"],
+    )
+    mail_message.body = "This is a test"
+    mail.send(mail_message)
+    return "Mail has sent"
